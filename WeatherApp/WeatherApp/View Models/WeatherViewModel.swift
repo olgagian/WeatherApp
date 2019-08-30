fb5754e98dbb1910cca2ba6a37faa41f4d3956b7
ioannis giannakidis <ioannis@MiniServer.local>
2019-08-30 15:37:15 +0300

    finished building the app

---
+++ WeatherApp/WeatherApp/View Models/WeatherViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherViewModel.swift
+//  WeatherApp
+//
+//  Created by ioannis giannakidis on 29/8/19.
+//  Copyright © 2019 ioannis giannakidis. All rights reserved.
+//
+
+import Foundation
